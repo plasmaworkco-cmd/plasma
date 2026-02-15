36a3c9c8c42c565dff3f3deaeb4e331e018845d8
Thunderbeast5 <163726116+Thunderbeast5@users.noreply.github.com>
2026-02-15 22:45:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function App() {
+  return (
+    <div className="min-h-screen bg-red-100">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
